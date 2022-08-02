eea05c4e0a1af2168ec4f621a6bc61b9d7ec6422
Mark Keller <8452750+keller00@users.noreply.github.com>
2022-08-01 19:18:10 -0700

    my changes on top of upstream

--- shipit.user.js
+++ shipit.user.js
@@ -1,34 +1,17 @@
 // ==UserScript==
 // @name         shipit
-// @namespace    http://asottile.com/
+// @namespace    http://markkeller.dev/
 // @version      0.2
-// @author       asottile
+// @author       keller00
 // @match        https://github.com/*
-// @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
 (function () {
-    var urls = [];
-
-    GM.xmlHttpRequest({
-        method: 'GET',
-        url: 'https://chriskuehl.github.io/shipit',
-        onload: function (resp) {
-            if (resp.status === 200) {
-                var parser = new DOMParser();
-                var html = parser.parseFromString(resp.response, 'text/html').body;
-                html.querySelectorAll('#shipit img').forEach(function (e) {
-                    urls.push(e.src);
-                });
-            }
-        }
-    });
-
     document.documentElement.addEventListener('click', function (e) {
         var msg;
-        if (urls.length && e.target.name === 'pull_request_review[event]' && e.target.value === 'approve') {
+        if (e.target.name === 'pull_request_review[event]' && e.target.value === 'approve') {
             msg = document.querySelector('#pull_request_review_body')
-            msg.value = `${msg.value}\n\n![](${urls[Math.floor(Math.random() * urls.length)]})`;
+            msg.value = `${msg.value}\n\n:ship: :ship:`;
         }
     });
 }());
